ca2703f07416f04922cf941ed3da59574c9acf14
Taiwo <taiwotunde32@gmail.com>
2022-09-29 11:26:09 +0100

    updates

--- javascript.js
+++ javascript.js
@@ -22,32 +22,44 @@ for (var i = 0; i < remove.length; i++) {
 
 
 
-let shopPrices = document.querySelectorAll("[ data-shopPrice ]")
-let itemQuantitys = document.querySelectorAll("[data-itemQuantity]");
 
- itemQuantitys.forEach( itemQuantity => {
-   itemQuantity.addEventListener("click", () =>{
-    let itemsSelected = (itemQuantity.value) * 20
-         console.log(itemsSelected)
+let quantity1 = document.getElementById("quantity1");
+let quantity2 = document.getElementById("quantity2");
+let quantity3 = document.getElementById("quantity3");
+
+let price1 = document.getElementById("price1");
+let price2 = document.getElementById("price2");
+let price3 = document.getElementById("price3");
+
+let totalPrice = document.getElementById("totalPrice");
+
+ quantity1.addEventListener('click', () =>{
+
+   var item1 = quantity1.value * 20
+   price1.innerText = item1
+
+
+   quantity2.addEventListener('click', () =>{
+    var item2 = quantity2.value * 20
+    price2.innerText  =    item2
+  
+  
+  
+    quantity3.addEventListener('click', () =>{
+      var item3 = quantity3.value * 20
+      price3.innerText  =  item3
        
+     
 
-     shopPrices.forEach( shopPrice => {
-        shopPrice.addEventListener("onchange", () =>{
-          let itemQuantity =itemsSelected * 20
-           console.log(itemQuantity)
-        })
-      
+  totalPrice.innerText =    item1 + item2 + item3
 
-       
-       
-      
-
-     })
-
-   
    })
-   
- })
+  
+})
+
+
+})
+
 
 
 function updateTotal() {
@@ -66,39 +78,32 @@ function updateTotal() {
 
 
 
-// let quantity1 = document.getElementById("quantity1");
-// let quantity2 = document.getElementById("quantity2");
-// let quantity3 = document.getElementById("quantity3");
-
-// let price1 = document.getElementById("price1");
-// let price2 = document.getElementById("price2");
-// let price3 = document.getElementById("price3");
-
-// let totalPrice = document.getElementById("totalPrice");
-
-//  quantity1.addEventListener('click', () =>{
-
-//    var item1 = quantity1.value * 20
-//    price1.innerText = item1
 
 
-//    quantity2.addEventListener('click', () =>{
-//     var item2 = quantity2.value * 20
-//     price2.innerText  =    item2
-  
-  
-  
-//     quantity3.addEventListener('click', () =>{
-//       var item3 = quantity3.value * 20
-//       price3.innerText  =  item3
+
+// let shopPrices = document.querySelectorAll("[ data-shopPrice ]")
+// let itemQuantitys = document.querySelectorAll("[data-itemQuantity]");
+
+//  itemQuantitys.forEach( itemQuantity => {
+//    itemQuantity.addEventListener("click", () =>{
+//     let itemsSelected = (itemQuantity.value) * 20
+//          console.log(itemsSelected)
        
-     
 
-//   totalPrice.innerText =    item1 + item2 + item3
+//      shopPrices.forEach( shopPrice => {
+//         shopPrice.addEventListener("onchange", () =>{
+//           let itemQuantity =itemsSelected * 20
+//            console.log(itemQuantity)
+//         })
+      
 
+       
+       
+      
+
+//      })
+
+   
 //    })
-  
-// })
-
-
-// })
+   
+//  })
